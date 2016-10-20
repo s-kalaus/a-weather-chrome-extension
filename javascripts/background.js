3703f8c107ec54ad85c7c3b787a78c1e542c4665
s-kalaus <sergey.kalaus@gmail.com>
2016-10-20 09:41:01 +0300

    Minus temperatures fix

--- javascripts/background.js
+++ javascripts/background.js
@@ -24,7 +24,7 @@ aWeather.run(['configService', '$interval', 'weatherService', function (configSe
                 badgeText.push('-');
             }
 
-            badgeText.push(Math.round(data.weather.main.temp));
+            badgeText.push(Math.abs(Math.round(data.weather.main.temp)));
 
             chrome.browserAction.setBadgeText({ text: badgeText.join('') });
 
